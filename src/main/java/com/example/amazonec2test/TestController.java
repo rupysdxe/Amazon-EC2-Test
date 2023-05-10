9e6f3abfb25ce3e209fa8167ac5e54d4817b3355
Rupesh Dangi <83266954+rupysdxe@users.noreply.github.com>
2023-05-10 21:25:55 +0545

    jhg

--- src/main/java/com/example/amazonec2test/TestController.java
+++ src/main/java/com/example/amazonec2test/TestController.java
@@ -1,2 +1,15 @@
-package com.example.amazonec2test;public class TestController {
+package com.example.amazonec2test;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+        @RequestMapping("/test")
+        public ResponseEntity<String> hello() {
+            return ResponseEntity.ok("Hello World!");
+        }
 }
